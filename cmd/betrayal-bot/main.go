38c517eb77405da78cdefffc6911b2309d1380f2
McCune1224 <alexmccune1224@gmail.com>
2023-10-25 19:30:12 -0400

    Nasty ugly butt ugly POC for command logger

--- cmd/betrayal-bot/main.go
+++ cmd/betrayal-bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -17,7 +18,6 @@ import (
 	"github.com/mccune1224/betrayal/internal/commands/view"
 	"github.com/mccune1224/betrayal/internal/data"
 	"github.com/mccune1224/betrayal/internal/discord"
-	"github.com/mccune1224/betrayal/internal/middlewares"
 	"github.com/mccune1224/betrayal/internal/util"
 	"github.com/zekrotja/ken"
 	"github.com/zekrotja/ken/state"
@@ -132,23 +132,13 @@ func main() {
 		new(commands.Kill),
 		new(commands.Revive),
 	)
-	err = app.betrayalManager.RegisterMiddlewares(new(middlewares.PermissionsMiddleware))
-	if err != nil {
-		log.Fatal(err)
-	}
 	// logger setup for slash commands and ken
-	loggerChID := "1108318770138714163"
+	// loggerChID := "1108318770138714163"
 	// testLoggerID := "1140968068705701898"
-	app.betrayalManager.Session().AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		// create log of template [invoker] [command] [timestamp]
-		msg := i.Member.User.Username + " - " + i.ApplicationCommandData().Name + " - " + util.GetEstTimeStamp()
-		msg = discord.Code(msg)
-		// send msg to logger channel
-		_, err := s.ChannelMessageSend(loggerChID, msg)
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
+	//
+	//
+
+	app.betrayalManager.Session().AddHandler(logHandler)
 	defer app.betrayalManager.Unregister()
 
 	err = bot.Open()
@@ -171,5 +161,79 @@ func main() {
 
 	if err := app.betrayalManager.Session().Close(); err != nil {
 		log.Fatal("error closing connection,", err)
+	}
+}
+
+// FIXME: this is a mess
+// May god have mercy on my soul for the following function
+func logHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// loggerChID := "1108318770138714163"
+	testLoggerID := "1140968068705701898"
+	// check for subcommand and if so fetch the subcommand name from the options
+	options := i.ApplicationCommandData().Options
+	optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+	for _, opt := range options {
+		optionMap[opt.Name] = opt
+	}
+
+	msg := ""
+
+	for _, opt := range options {
+		if o, ok := optionMap[opt.Name]; ok {
+			switch o.Type {
+			default:
+				continue
+			case discordgo.ApplicationCommandOptionString:
+				msg += fmt.Sprintf("%s: %s, ", o.Name, o.StringValue())
+			case discordgo.ApplicationCommandOptionInteger:
+				msg += fmt.Sprintf("%s: %d, ", o.Name, o.IntValue())
+			case discordgo.ApplicationCommandOptionBoolean:
+				msg += fmt.Sprintf("%s: %t, ", o.Name, o.BoolValue())
+			case discordgo.ApplicationCommandOptionUser:
+				msg += fmt.Sprintf("%s: %s, ", o.Name, o.UserValue(s).Username)
+			case discordgo.ApplicationCommandOptionChannel:
+				msg += fmt.Sprintf("%s: %s, ", o.Name, o.ChannelValue(s).Name)
+			case discordgo.ApplicationCommandOptionRole:
+				continue
+			case discordgo.ApplicationCommandOptionMentionable:
+				continue
+			case discordgo.ApplicationCommandOptionSubCommand:
+				// do the same thing as above but for subcommands
+				subOptions := o.Options
+				subOptionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(subOptions))
+				for _, subOpt := range subOptions {
+					subOptionMap[subOpt.Name] = subOpt
+				}
+				for _, subOpt := range subOptions {
+					if so, ok := subOptionMap[subOpt.Name]; ok {
+						switch so.Type {
+
+						case discordgo.ApplicationCommandOptionString:
+							msg += fmt.Sprintf("%s: %s, ", so.Name, so.StringValue())
+						case discordgo.ApplicationCommandOptionInteger:
+							msg += fmt.Sprintf("%s: %d, ", so.Name, so.IntValue())
+						case discordgo.ApplicationCommandOptionBoolean:
+							msg += fmt.Sprintf("%s: %t, ", so.Name, so.BoolValue())
+						case discordgo.ApplicationCommandOptionUser:
+							msg += fmt.Sprintf("%s: %s, ", so.Name, so.UserValue(s).Username)
+						case discordgo.ApplicationCommandOptionChannel:
+							msg += fmt.Sprintf("%s: %s, ", so.Name, so.ChannelValue(s).Name)
+						case discordgo.ApplicationCommandOptionRole:
+							continue
+						case discordgo.ApplicationCommandOptionMentionable:
+							continue
+						default:
+							continue
+						}
+					}
+				}
+			}
+		}
+	}
+	// get invoker name
+	logOutput := fmt.Sprintf("%s - /%s '%s' - %s", i.Member.User.Username, i.ApplicationCommandData().Name, msg, util.GetEstTimeStamp())
+	_, err := s.ChannelMessageSend(testLoggerID, discord.Code(logOutput))
+	if err != nil {
+		log.Println(err)
 	}
 }
