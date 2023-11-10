4161dc7bffca1c0c810694eb6d28b474f2501403
McCune1224 <alexmccune1224@gmail.com>
2023-11-09 20:46:42 -0500

    helps to turn commands back on....

--- cmd/betrayal-bot/main.go
+++ cmd/betrayal-bot/main.go
@@ -14,7 +14,10 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/joho/godotenv/autoload"
 	_ "github.com/lib/pq"
+	"github.com/mccune1224/betrayal/internal/commands"
 	"github.com/mccune1224/betrayal/internal/commands/inventory"
+	roll "github.com/mccune1224/betrayal/internal/commands/luck"
+	"github.com/mccune1224/betrayal/internal/commands/view"
 	"github.com/mccune1224/betrayal/internal/data"
 	"github.com/mccune1224/betrayal/internal/discord"
 	"github.com/mccune1224/betrayal/internal/util"
@@ -126,20 +129,20 @@ func main() {
 	app.betrayalManager.Unregister()
 
 	tally := app.RegisterBetrayalCommands(
-		// new(commands.Test),
+		new(commands.Test),
 		new(inventory.Inventory),
-		// new(roll.Roll),
-		// new(commands.ActionFunnel),
-		// new(view.View),
-		// new(commands.Buy),
-		// new(commands.List),
-		// new(commands.Insult),
-		// new(commands.Ping),
-		// new(commands.Vote),
-		// new(commands.Kill),
-		// new(commands.Revive),
-		// new(commands.Setup),
-		// new(commands.Alliance),
+		new(roll.Roll),
+		new(commands.ActionFunnel),
+		new(view.View),
+		new(commands.Buy),
+		new(commands.List),
+		new(commands.Insult),
+		new(commands.Ping),
+		new(commands.Vote),
+		new(commands.Kill),
+		new(commands.Revive),
+		new(commands.Setup),
+		new(commands.Alliance),
 	)
 
 	app.betrayalManager.Session().AddHandler(logHandler)
