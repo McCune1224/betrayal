d881afe9917a94bee1b8f7f13d15239f4e4659bb
McCune1224 <alexmccune1224@gmail.com>
2023-11-11 21:07:07 -0500

    migrations are long gone lol

--- migrations/000002_create_insults_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS insults (
-    id SERIAL PRIMARY KEY,
-    author_id TEXT NOT NULL,
-    insult TEXT NOT NULL UNIQUE,
-    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
-);
