33ed4e25206f900ee8070a069f12ec8f47a84237
McCune1224 <alexmccune1224@gmail.com>
2023-12-20 21:43:58 -0500

    Reformat of help admin inventory command

--- internal/commands/help/admin_messages.go
+++ internal/commands/help/admin_messages.go
@@ -6,18 +6,25 @@ import (
 
 func adminInventoryEmbed() *discordgo.MessageEmbed {
 	msg := &discordgo.MessageEmbed{
-		Title: "Inventory",
-		Description: "Overall on how to manage and update player inventories using `/inv`. *Most* inv command will fall under the template of /inv [category] [add/remove/set] [thing]. This command can be used in a player's confessional channel or in a channel that has been whitelisted for inventory commands.\n " +
-			"You can add a channel to the whitelist by using the `/whitelist` command. ** One key thing to note is if you are in a player's confessional channel, you may omit the `player` argument and it will default to the player who's confessional channel you are in.**",
-
+		Title:       "Inventory",
+		Description: "Overall on how to manage and update player inventories using `/inv`. Biggest takeaway is that modifying a player's inventory generally follows the flow of `/inv [category] [add/remove/set] [player]`.",
 		Fields: []*discordgo.MessageEmbedField{
 			{
-				Value: "For status and effect fields, you an also include an optional duration `argument` within their add argument. This follows a simple string format of human-readable duration string (e.g., '1h30m') to schedule a status or effect to expire after a certain amount of time. If no duration is specified, the status or effect will be permanent." +
-					"If you remove a status or effect that was scheduled to expire, it will be removed from the scheduler to avoid accidental future deletions." +
-					"For sake of minimal overhead, If you wish to 'extend' the duration of something, it is recommended to remove it and re-add it with the new duration.",
+				Value: "`/inv [category] [add/remove/set] [player]`. This is how you add/remove/set items to a player's inventory. Can only be done in a player's confessional channel or a whitelisted channel. *(player argument is not mandatory if you're already in the player's confessional)*",
 			},
 			{
-				Value: "When adding or removing items to a player's inventory you may do so in either their confessional channel, or within a created whitelist channel. You can whitelist a channel by using the `/whitelist` command. If you are in a , you may omit the `player` argument and it will default to the player who's confessional channel you are in.",
+				Value: ">>> **status** and **effect** fields also include an optional argument `duration` when adding. It uses a string format of human-readable duration string (e.g., '1h30m') to schedule a status or effect to expire." +
+					"For sake of minimal overhead, If you wish to 'extend' the duration of something, it is recommended to remove it and re-add it with the new duration.",
+			},
+			{},
+			{
+				Value: "`/inv create [target]`. Create an inventory for desired player (this command should be issued within the player's confessional as it will pin the inventory to this channel).",
+			},
+			{
+				Value: "`/inv delete [target]`. Delete an inventory for desired player. Will also delete the pinned inventory within the player's confessional channel if it exists.",
+			},
+			{
+				Value: "`/inv whitelist [add/remove]`. Add or Remove from the Whitelist for a channel for allowing inventory commands. This will allow you to issue inventory commands in the channel.",
 			},
 		},
 	}
